a3668b65433656f178a78583b036ef92966487f3
dungeoncringemaster <162499841+dungeoncringemaster@users.noreply.github.com>
2024-03-06 22:58:08 +0300

    hm

---
+++ lesson1_hw1/lesson1_hw1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <locale.h>
+/*
+     Let’s
+       go
+            to walk*/
+
+int main(int argc, char **argv)
+{   setlocale(LC_ALL,"Rus");
+       printf("  Let’s\n");
+   printf("       go\n");
+   printf("         to walk\n");
+
+    return 0;
+}
+
---
+++ lesson1_hw2/lesson1_hw2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <locale.h>
+/*
+     *
+    ***
+   *****
+  *******
+   HH HH
+   ZZZZZ
+*/
+
+int main(int argc, char **argv)
+
+{   setlocale(LC_ALL,"Rus");
+        printf("    *   \n");
+        printf("   ***  \n");
+        printf("  ***** \n");
+        printf(" *******\n");
+        printf("  HH HH \n");
+        printf("  ZZZZZ \n");
+
+    return 0;
+}
+
